3d3c4e0b63f23672386554274f7c185a82857a25
MelonPowerr <42519942+MelonPowerr@users.noreply.github.com>
2022-12-15 09:33:03 +0300

    Delete Лаб3 directory

--- Лаб3/prog_uravnenie.m
+++
@@ -1,5 +0,0 @@
-for x=0:10
-for y=0:10
-  z=(sin(x)^2+cos(y)^2)*2
-endfor
-endfor
--- Лаб3/welldone.pl
+++
@@ -1,6 +0,0 @@
-man(s,otl).
-man(k,tr).
-woman(l,hor).
-woman(r,tr).
-w(M):-man(M,otl);woman(M,otl);man(M,hor);woman(M,hor).
-isman(M):-w(M),man(M,_). 
